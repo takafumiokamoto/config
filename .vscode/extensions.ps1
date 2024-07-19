f32eda9486f793ceeb1d6b2f751418f8630e0efc
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2024-07-19 04:40:42 -0700

    拡張機能更新

--- .vscode/extensions.ps1
+++ .vscode/extensions.ps1
@@ -1,2 +1,17 @@
 code --install-extension albert.tabout
+code --install-extension esbenp.prettier-vscodcode --install-extension albert.tabout
+code --install-extension davidanson.vscode-markdownlint
 code --install-extension esbenp.prettier-vscode
+code --install-extension github.github-vscode-theme
+code --install-extension mhutchie.git-graph
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-server
+code --install-extension pkief.material-icon-theme
+code --install-extension vscodevim.vim
+
--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 ## vscode extension
 
 export
+
 ```
 code --list-extensions | % { "code --install-extension $_" } |Out-File extensions.ps1
 ```
 
 import
+
 ```
 powershell extensions.ps1
 ```
-
